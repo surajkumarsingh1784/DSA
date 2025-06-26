e6ba2be204f571997fe205b4867cebb57956d550
Suraj Kumar Singh <surajkumarsingh2022@vitbhopal.ac.in>
2025-06-27 03:37:50 +0530

    Time: 34 ms (31.02%) | Memory: 138.5 MB (94.86%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& arr, int k) {
+        if(k == 1) return arr;
+        int n = arr.size();
+        int ngi[n]; 
+        stack<int> st;
+        ngi[n - 1] = n;
+        st.push(n - 1);      
+        for (int i = n - 2; i >= 0; i--) {
+            while (st.size() > 0 && arr[st.top()] <= arr[i]) {
+                st.pop();
+            }
+            if (st.size() == 0) ngi[i] = n;
+            else ngi[i] = st.top();
+            st.push(i);
+        }
+         vector<int> ans;
+        int j = 0;
+        for (int i = 0; i <= n - k; i++) {
+            if (j < i) j = i;
+            while (ngi[j] < i + k) {
+                j = ngi[j]; // jump to next greater
+            }
+            ans.push_back(arr[j]);
+        }
+
+        return ans;
+    }
+};
