e0c467dafaf62ecf2d7a4ca27d11cc6c9b42357a
Suraj Kumar Singh <surajkumarsingh2022@vitbhopal.ac.in>
2025-07-01 01:44:50 +0530

    Time: 0 ms (100.00%) | Memory: 26.1 MB (50.40%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int left = 0, right = n - 1;
+        int leftMax = 0, rightMax = 0;
+        int trappedWater = 0;
+
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    trappedWater += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    trappedWater += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+
+        return trappedWater;
+    }
+};
