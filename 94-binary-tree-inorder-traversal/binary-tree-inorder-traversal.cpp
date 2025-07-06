50965b5497772e46b9a52d1da724da64bac663af
Suraj Kumar Singh <surajkumarsingh2022@vitbhopal.ac.in>
2025-07-07 02:11:31 +0530

    Time: 1 ms (5.84%) | Memory: 10.8 MB (63.85%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -13,7 +13,6 @@ class Solution {
 public:
 void inorder(TreeNode* root,vector<int>& ans){
     if(root==NULL) return;
-    
     inorder(root->left,ans);
     ans.push_back(root->val);
     inorder(root->right,ans);
