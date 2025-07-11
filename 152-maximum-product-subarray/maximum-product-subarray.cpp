5f279b7e9860e1fd0eb753a648b44e10269a1ea4
Suraj Kumar Singh <surajkumarsingh2022@vitbhopal.ac.in>
2025-07-12 02:52:07 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (21.13%) - LeetSync

---
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+         int maxProd = nums[0];
+        int currMax = nums[0];
+        int currMin = nums[0];
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] < 0) {
+                swap(currMax, currMin); // because multiplying with -ve reverses roles
+            }
+
+            currMax = max(nums[i], currMax * nums[i]);
+            currMin = min(nums[i], currMin * nums[i]);
+
+            maxProd = max(maxProd, currMax);
+        }
+
+        return maxProd;
+    }
+};
