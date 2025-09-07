a09aa99b9bba159b6cc5a2e95df122d4fa3c05de
Suraj Kumar Singh <surajkumarsingh2022@vitbhopal.ac.in>
2025-09-07 18:55:01 +0530

    Time: 4 ms (32.33%) | Memory: 8.7 MB (40.53%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isPalindrome(int n) {
+          long long revNum = 0;
+    // Create a duplicate variable to
+    // store the original number
+    int dup = n;
+    // Iterate through each digit of
+    // the number until it becomes 0
+    while (n > 0) {
+        // Extract the last
+        // digit of the number
+        int ld = n % 10;
+        // Build the reverse number
+        // by appending the last digit
+        revNum = (revNum * 10) + ld;
+        // Remove the last digit
+        // from the original number
+        n = n / 10;
+    }
+    // Check if the original number
+    // is equal to its reverse
+    if (dup == revNum) {
+        // If equal, return true
+        // indicating it's a palindrome
+        return true;
+    } else {
+        // If not equal, return false
+        // indicating it's not a palindrome
+        return false;
+    }
+    }
+};
