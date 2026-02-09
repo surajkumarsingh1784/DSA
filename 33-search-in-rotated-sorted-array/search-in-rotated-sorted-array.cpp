3d22c3665d938c94f7676643d33af2c980935956
Suraj Kumar Singh <surajkumarsingh2022@vitbhopal.ac.in>
2026-02-09 09:26:32 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (33.63%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,30 +1,11 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int x) {
-        int low = 0, high = nums.size() - 1;
-
-        while (low <= high) {
-            int mid = (low + high) / 2;
-
-            if (nums[mid] == x) return mid;
-
-            // Check which part is sorted
-            if (nums[low] <= nums[mid]) { 
-                // Left part is sorted
-                if (x >= nums[low] && x < nums[mid])
-                    high = mid - 1; // Search left side
-                else
-                    low = mid + 1; // Search right side
-            } 
-            else { 
-                // Right part is sorted
-                if (x > nums[mid] && x <= nums[high])
-                    low = mid + 1; // Search right side
-                else
-                    high = mid - 1; // Search left side
+    int search(vector<int>& nums, int target) {
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==target){
+                return i;
             }
         }
-
-        return -1; // Not found
+        return -1;
     }
 };
