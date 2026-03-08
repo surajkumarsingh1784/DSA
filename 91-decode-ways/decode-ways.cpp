4aa726c18cb26978eae53e6c2229a9adb11cf809
Suraj Kumar Singh <surajkumarsingh2022@vitbhopal.ac.in>
2026-03-08 23:05:48 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (29.95%) - LeetSync

---
+++ 91-decode-ways/decode-ways.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.size();
+        if(s[0] == '0') return 0;
+
+        vector<int> dp(n+1);
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i = 2; i <= n; i++){
+            if(s[i-1] != '0')
+                dp[i] += dp[i-1];
+
+            int two = stoi(s.substr(i-2,2));
+            if(two >= 10 && two <= 26)
+                dp[i] += dp[i-2];
+        }
+
+        return dp[n];
+    }
+};
